40f034d74ce2f3acec0556458bd0505b449265a4
alanhidayat33 <alanarbanihidayat92@gmail.com>
2022-11-02 16:29:56 +0700

    third commit

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -1,10 +1,10 @@
 <?php
-
+  
 namespace App\Http\Middleware;
-
+  
 use Closure;
 use Illuminate\Http\Request;
-
+  
 class UserAccess
 {
     /**
@@ -14,12 +14,13 @@ class UserAccess
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, $userType)
     {
-        if(auth () -> user()->type == $userType){
-            return $next($request);    
+        if(auth()->user()->type == $userType){
+            return $next($request);
         }
-        
-        return response()->json(['Tidak bisa mengakses halaman ini']);
+          
+        return response()->json(['You do not have permission to access for this page.']);
+        /* return response()->view('errors.check-permission'); */
     }
 }
--- database/seeders/CreateUsersSeeder.php
+++ database/seeders/CreateUsersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class CreateUsersSeeder extends Seeder
 {
@@ -14,6 +15,29 @@ class CreateUsersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = [
+            [
+               'name'=>'Admin User',
+               'email'=>'admin@itsolutionstuff.com',
+               'type'=>1,
+               'password'=> bcrypt('123456'),
+            ],
+            [
+               'name'=>'Manager User',
+               'email'=>'manager@itsolutionstuff.com',
+               'type'=> 2,
+               'password'=> bcrypt('123456'),
+            ],
+            [
+               'name'=>'User',
+               'email'=>'user@itsolutionstuff.com',
+               'type'=>0,
+               'password'=> bcrypt('123456'),
+            ],
+        ];
+    
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
     }
 }
