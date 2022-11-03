e6d12b83a7dd47e811f7eceb553a7c3f595b933d
alanhidayat33 <alanarbanihidayat92@gmail.com>
2022-11-03 12:05:38 +0700

    fifth commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-center">{{ __('Login Surat.in') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        <div class="row mb-2">
+                            <div class="col-md-16">
+                                <img src="{{ URL::to('/assets/img/logo.png') }}" class="img-responsive mx-auto d-block" alt="logo" srcset="">
+                            </div>    
+                        </div>
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Login Mahasiswa') }}
                 </div>
             </div>
         </div>
