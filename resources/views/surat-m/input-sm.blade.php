e474ea08e4c88d4a984921e6cf920162ffa4109f
dzikrimutawakkil <dzikrimutawakkil54@gmail.com>
2022-11-24 16:03:27 +0700

    tambah surat

--- resources/views/surat-m/input-sm.blade.php
+++ resources/views/surat-m/input-sm.blade.php
@@ -4,73 +4,60 @@
 <div class="container-fluid">
     <div class="row mt-4 mx-lg-3">
         <div class="col-lg-3 pe-lg-5 px-1" id="navbarNavAltMarkup">
-                @include('component.sidebar')
+            @include('component.sidebar')
         </div>
-        <div class="col-lg-9 mt-4">
-            <div class="row">
-                <div class="col">
-                    <div class="row">
-                        <div class="page-header">
-                            <h3 class="page-title">Tambah Surat Masuk</h3>
-                        </div>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="card-body">
-                            <form action="/save-sm" method="POST" class="forms-sample" enctype="multipart/form-data">
-                                {{ csrf_field() }}
-                                <div class="form-group">
-                                    <label for="inputnomorsurat">Nomor Surat</label>
-                                    <input type="text" name="noSmasuk" class="" placeholder="Nomor Surat" value="{{ old('noSmasuk') }}">
-                                    @error('noSmasuk')
-                                        <p class="text-danger pt-1">{{ $message}}</p>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="tanggal">Tanggal Surat</label>
-                                    <input type="date" name="tglMasuk" class="" placeholder="Tanggal Surat" value="{{ old('tglMasuk') }}">
-                                    @error('tglMasuk')
-                                        <p class="text-danger pt-1">{{ $message}}</p>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="tanggal">Pengirim</label>
-                                    <input type="text" name="pengirim" class="" placeholder="Nama Pengirim" value="{{ old('pengirim') }}">
-                                    @error('pengirim')
-                                        <p class="text-danger pt-1">{{ $message}}</p>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlSelect2">jenis Surat</label>
-                                    <select class="form-control" id="exampleFormControlSelect2" name="jenisSurat_id"
-                                        value="{{ old('jenisSurat_id') }}">
-                                        <option selected disabled>Select one</option>
-                                        @foreach ($data as $x)
-                                            <option value="{{ $x->id }}">{{ $x->keterangan }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('jenisSurat_id')
-                                        <p class="text-danger pt-1"><small> {{ $message }}</small></p>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="tanggal">Upload File</label>
-                                    <input type="file" name="file" value="{{ old('file') }}">
-                                    <div class="input-group">
-                                        <input type="text" disabled placeholder="Upload File">
-                                        <span class="input-group-append">
-                                            <button type="file-upload-browse button">Upload</button>
-                                        </span>
-                                    </div>
-                                    @error('file')
-                                        <p class="text-danger pt-1">{{ $message}}</p>
-                                    @enderror
-                                </div>
-                                <button type="submit" class="btn">Tambah Surat</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+        <div class="col-lg-9 bg-white pt-4 shadow-sm" style="border-radius:15px">
+            <div class="page-header mb-4">
+                <h3 class="page-title">Tambah Surat Masuk</h3>
             </div>
+            <form action="/save-sm" method="POST" class="forms-sample row" enctype="multipart/form-data">
+
+                {{ csrf_field() }}
+                <div class="col-md-4">
+                    <label for="inputZip" class="form-label">Nomer Surat</label>
+                    <input type="text" name="noSmasuk" class="form-control" value="{{ old('noSmasuk') }}">
+                    @error('noSmasuk')
+                    <div id="emailHelp" class="form-text text-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="col-md-4">
+                    <label for="inputState" class="form-label">Jenis Surat</label>
+                    <select class="form-select" id="inputState" name="jenisSurat_id" value="{{ old('jenisSurat_id') }}">
+                        @foreach ($data as $x)
+                        <option value="{{ $x->id }}">{{ $x->keterangan }}</option>
+                        @endforeach
+                    </select>
+                    @error('jenisSurat_id')
+                    <div id="emailHelp" class="form-text text-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="col-md-4">
+                    <label for="inputCity" class="form-label">Date</label><br>
+                    <input type="date" name="tglMasuk" class="form-control" placeholder="Tanggal Surat"
+                        value="{{ old('tglMasuk') }}">
+                    @error('tglMasuk')
+                    <div id="emailHelp" class="form-text text-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="my-3">
+                    <label for="exampleInputEmail1" class="form-label">Nama Pengirim</label>
+                    <input type="text" name="pengirim" class="form-control" value="{{ old('pengirim') }}">
+                    @error('pengirim')
+                    <div id="emailHelp" class="form-text text-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="tanggal">Upload File</label>
+                    <!-- <input type="file" name="file" value="{{ old('file') }}"> -->
+                    <input class="form-control" type="file" name="file" value="{{ old('file') }}">
+                    @error('file')
+                    <div id="emailHelp" class="form-text text-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="mt-4">
+                    <button type="submit" class="btn btn-primary">Tambah Surat</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/surat-m/view-sm.blade.php
+++ resources/views/surat-m/view-sm.blade.php
@@ -4,78 +4,74 @@
 <div class="container-fluid">
     <div class="row mt-4 mx-lg-3">
         <div class="col-lg-3 pe-lg-5 px-1" id="navbarNavAltMarkup">
-                @include('component.sidebar')
+            @include('component.sidebar')
         </div>
         <div class="col-lg-9 bg-white pt-4 shadow-sm" style="border-radius:15px">
             <div class="row">
-                <div class="">
-                    <div class="row">
-                        <div class="page-header d-flex justify-content-between">
-                            <h3 class="page-title fw-bold">Surat Masuk</h3>
-                            @if (auth()->user()->type == 'Admin')
-                                <a href="input-sm" type="button" class="btn btn-primary">
-                                    <i class="bi bi-envelope-plus"></i> Tambah Surat</a>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="row mt-2 p-3  bg-white">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th> Nomor Surat </th>
-                                    <th> Tanggal Surat </th>
-                                    <th> Pengirim </th>
-                                    <th> Jenis Surat</th>
-                                    <th> File </th>
-                                    <th> Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data as $x)
-                                    <tr>
-                                        <td>{{ $x->id }}</td>
-                                        <td>{{ $x->noSmasuk }}</td>
-                                        <td>{{ $x->tglMasuk }}</td>
-                                        <td>{{ $x->pengirim }}</td>
-                                        <td>{{ $x->jenisSurat['keterangan'] }}</td>
-                                        <td>
-                                            @empty($x->file)
-                                                <a type="button" href="/edit-sm/{{ $x->id }}"
-                                                    class="btn btn-sm btn-success" data-toggle="tooltip"
-                                                    data-placement="top" title="Edit">
-                                                    <i class="bi bi-file-earmark-arrow-down-fill"></i>
-                                                    Download
-                                                </a>
-                                            @else
-                                                <span class="btn btn-sm btn-warning"><i class="bi bi-file-earmark-excel-fill"></i> Tidak ada </span>
-                                            @endempty
-                                        </td>
-                                        <td>
-
-                                            @if (auth()->user()->type == 'Admin')
-                                                <a type="button" href="/edit-sm/{{ $x->id }}"
-                                                    class="btn btn-sm btn-primary" data-toggle="tooltip"
-                                                    data-placement="top" title="Edit">
-                                                    <i class="bi bi-pencil-fill"></i>
-                                                </a>
-                                                <a type="button" href="/hapus-sm/{{ $x->id }}"
-                                                    onclick="return confirm('Apakah anda yakin menghapus data?')"
-                                                    class="btn btn-sm btn-danger"
-                                                    data-toggle="tooltip" data-placement="top" title="Delete">
-                                                    <i class="bi bi-trash-fill"></i>
-                                                </a>
-                                            @endif
-
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>    
-                    </div>
+                <div class="page-header d-flex justify-content-between px-4">
+                    <h3 class="page-title fw-bold ">Surat Masuk</h3>
+                    @if (auth()->user()->type == 'Admin')
+                    <a href="input-sm" type="button" class="btn btn-primary">
+                        <i class="bi bi-envelope-plus"></i> Tambah Surat</a>
+                    @endif
                 </div>
             </div>
+            <div class="row mt-2 p-3  bg-white">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th> Nomor Surat </th>
+                            <th> Tanggal Surat </th>
+                            <th> Pengirim </th>
+                            <th> Jenis Surat</th>
+                            <th> File </th>
+                            <th> Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="overflow-scroll">
+                        @foreach ($data as $x)
+                        <tr>
+                            <td>{{ $x->id }}</td>
+                            <td>{{ $x->noSmasuk }}</td>
+                            <td>{{ $x->tglMasuk }}</td>
+                            <td>{{ $x->pengirim }}</td>
+                            <td>{{ $x->jenisSurat['keterangan'] }}</td>
+                            <td>
+                                @empty($x->file)
+                                <a type="button" href="/edit-sm/{{ $x->id }}" class="btn btn-sm btn-success"
+                                    data-toggle="tooltip" data-placement="top" title="Edit">
+                                    <i class="bi bi-file-earmark-arrow-down-fill"></i>
+                                    Download
+                                </a>
+                                @else
+                                <span class="btn btn-sm btn-warning"><i class="bi bi-file-earmark-excel-fill"></i> Tidak
+                                    ada </span>
+                                @endempty
+                            </td>
+                            <td>
+
+                                @if (auth()->user()->type == 'Admin')
+                                <a type="button" href="/edit-sm/{{ $x->id }}" class="btn btn-sm btn-primary"
+                                    data-toggle="tooltip" data-placement="top" title="Edit">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a type="button" href="/hapus-sm/{{ $x->id }}"
+                                    onclick="return confirm('Apakah anda yakin menghapus data?')"
+                                    class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top"
+                                    title="Delete">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                                @endif
+
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     </div>
 </div>
 
