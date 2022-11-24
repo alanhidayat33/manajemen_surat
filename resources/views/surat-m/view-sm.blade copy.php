472195c303e76ba5537ec692417c0211719d4b66
dzikrimutawakkil <dzikrimutawakkil54@gmail.com>
2022-11-24 13:44:34 +0700

    tabel sm

--- resources/views/surat-m/view-sm.blade copy.php
+++ resources/views/surat-m/view-sm.blade copy.php
@@ -6,29 +6,29 @@
         <div class="col-lg-3 pe-lg-5 px-1" id="navbarNavAltMarkup">
                 @include('component.sidebar')
         </div>
-        <div class="col-lg-9 bg-white pt-4 shadow-sm" style="border-radius:15px">
+        <div class="col-lg-9 mt-4">
             <div class="row">
-                <div class="">
+                <div class="col">
                     <div class="row">
-                        <div class="page-header d-flex justify-content-between">
-                            <h3 class="page-title fw-bold">Surat Masuk</h3>
-                            @if (auth()->user()->type == 'Admin')
-                                <a href="input-sm" type="button" class="btn btn-primary">
-                                    <i class="bi bi-envelope-plus"></i> Tambah Surat</a>
-                            @endif
+                        <div class="page-header">
+                            <h3 class="page-title">Surat Masuk</h3>
+                                @if (auth()->user()->type == 'Admin')
+                                        <a href="input-sm" type="button" class="btn btn-gradient-primary btn-icon-text btn-sm">
+                                            <i class="bi bi-envelope-plus"></i> Tambah Surat</a>
+                                @endif
                         </div>
                     </div>
-                    <div class="row mt-2 p-3  bg-white">
-                        <table class="table table-striped">
+                    <div class="row mt-2">
+                    <table id="example1" class="table table-bordered bg-warning">
                             <thead>
                                 <tr>
                                     <th>No</th>
                                     <th> Nomor Surat </th>
-                                    <th> Tanggal Surat </th>
+                                    <th class="col-md-1"> Tanggal Surat </th>
                                     <th> Pengirim </th>
                                     <th> Jenis Surat</th>
-                                    <th> File </th>
-                                    <th> Action</th>
+                                    <th class="col-md-1"> File </th>
+                                    <th class="col-md-1"> Action</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -41,27 +41,28 @@
                                         <td>{{ $x->jenisSurat['keterangan'] }}</td>
                                         <td>
                                             @empty($x->file)
-                                                <a type="button" href="/edit-sm/{{ $x->id }}"
-                                                    class="btn btn-sm btn-success" data-toggle="tooltip"
-                                                    data-placement="top" title="Edit">
-                                                    <i class="bi bi-file-earmark-arrow-down-fill"></i>
-                                                    Download
-                                                </a>
+                                                <span class="badge badge-danger">Tidak ada</span>
                                             @else
-                                                <span class="btn btn-sm btn-warning"><i class="bi bi-file-earmark-excel-fill"></i> Tidak ada </span>
+                                                <span class="badge badge-success">Ada</span>
                                             @endempty
                                         </td>
+                                        <!-- <td> <img src="{{ $x->file }}" width="100px" height="auto" alt="file"></td> -->
                                         <td>
+                                            <a type="button" href="{{ $x->file }}" download
+                                                class="btn-sm btn-inverse-info btn-rounded m-lg-1" data-toggle="tooltip"
+                                                data-placement="top" title="Download File">
+                                                <i class="bi bi-file-earmark-arrow-down-fill"></i>
+                                            </a>
 
                                             @if (auth()->user()->type == 'Admin')
                                                 <a type="button" href="/edit-sm/{{ $x->id }}"
-                                                    class="btn btn-sm btn-primary" data-toggle="tooltip"
+                                                    class="btn-sm btn-inverse-dark btn-rounded m-lg-1" data-toggle="tooltip"
                                                     data-placement="top" title="Edit">
-                                                    <i class="bi bi-pencil-fill"></i>
+                                                    <i class="bi bi-pencil-square"></i>
                                                 </a>
                                                 <a type="button" href="/hapus-sm/{{ $x->id }}"
                                                     onclick="return confirm('Apakah anda yakin menghapus data?')"
-                                                    class="btn btn-sm btn-danger"
+                                                    class="btn-sm btn-inverse-danger btn-rounded m-lg-1"
                                                     data-toggle="tooltip" data-placement="top" title="Delete">
                                                     <i class="bi bi-trash-fill"></i>
                                                 </a>
